e82da9b90af175cfde0a2261776949a074ec49a5
Michael-jony <120747815@qq.com>
2017-07-01 07:42:51 +0800

    AtomicX

---
+++ basiccommon/src/main/java/com/michjony/basic/util/thread/VolatileAtomic.java
@@ -0,0 +1,38 @@
+package com.michjony.basic.util.thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class VolatileAtomic extends 	Thread{
+
+	private static volatile AtomicInteger count = new AtomicInteger(0);
+	
+	private static void addCount(){
+		for (int i = 0; i < 1000; i++) {
+			//非线程安全
+			count.incrementAndGet();
+		}
+		System.out.println(count);
+	}
+	
+	public void run(){
+		addCount();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		VolatileAtomic[] arr = new VolatileAtomic[10];
+		for (int i = 0; i <10 ; i++) {
+			arr[i] = new VolatileAtomic();
+		}
+		for (int i = 0; i <10 ; i++) {
+			arr[i].start();
+		}
+		
+		//等待这些线程运行完成
+		for (int i = 0; i <10 ; i++) {
+			arr[i].join();
+		}
+		System.out.println("---" + count);
+		
+	}
+}
+
---
+++ basiccommon/src/main/java/com/michjony/basic/util/thread/VolatileNoAtomic.java
@@ -0,0 +1,35 @@
+package com.michjony.basic.util.thread;
+
+
+public class VolatileNoAtomic extends 	Thread{
+
+	private static volatile int count ;
+	
+	private static void addCount(){
+		for (int i = 0; i < 1000; i++) {
+			//非线程安全
+			count ++ ;
+		}
+		System.out.println(count );
+	}
+	
+	public void run(){
+		addCount();
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		VolatileNoAtomic[] arr = new VolatileNoAtomic[10];
+		for (int i = 0; i <10 ; i++) {
+			arr[i] = new VolatileNoAtomic();
+		}
+		for (int i = 0; i <10 ; i++) {
+			arr[i].start();
+		}
+		//等待这些线程运行完成
+		for (int i = 0; i <10 ; i++) {
+			arr[i].join();
+		}
+		System.out.println("---" + count);
+	}
+}
+
