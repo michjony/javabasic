c8a8d53dbea49558bc555f84ad1621ec7d3a7c97
Michael-jony <120747815@qq.com>
2017-07-15 20:54:55 +0800

    init

---
+++ basiccommon/src/main/java/com/michjony/basic/util/netty/stickyandunpacked/package-info.java
@@ -0,0 +1,10 @@
+/**   
+* @Title: package-info.java 
+* @Package com.michjony.basic.util.netty.stickyandunpacked 
+* @author Michael-jony  
+* @date 2017年7月15日 下午8:51:42 
+* @version V1.0   
+*/
+
+
+package com.michjony.basic.util.netty.stickyandunpacked;
