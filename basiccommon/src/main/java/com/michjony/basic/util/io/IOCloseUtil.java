b953de9fa4bd18e783662f53dc82e1ce04186f30
mqzhao <mqzhao@xx.com>
2017-11-24 18:29:21 +0800

    IO释放

--- basiccommon/src/main/java/com/michjony/basic/util/io/IOCloseUtil.java
+++ basiccommon/src/main/java/com/michjony/basic/util/io/IOCloseUtil.java
@@ -1,14 +1,25 @@
 package com.michjony.basic.util.io;
 
-public class IOCloseUtil{
+public class IOCloseUtil {
 
-	public static void close(AutoCloseable t){
-		if(t!=null){
+	public static void close(AutoCloseable t) {
+		if (t != null) {
 			try {
 				t.close();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	public static <T extends AutoCloseable> void closeAuto(T t) {
+		if (t == null) {
+			return;
+		}
+		try {
+			t.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
